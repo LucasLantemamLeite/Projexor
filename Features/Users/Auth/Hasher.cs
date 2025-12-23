ac48e6b599319fe3b930545bfa4d41076c54d813
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-12-23 13:22:39 -0300

    feat: cria o serviço de Hash usando argon2id para segurança e performance

---
+++ Features/Users/Auth/Hasher.cs
@@ -0,0 +1,50 @@
+using System.Security.Cryptography;
+using System.Text;
+using Konscious.Security.Cryptography;
+
+namespace Stokify.Features.Users.Auth;
+
+public static class Hasher
+{
+    private readonly static int Memory = 1024 * 128;
+    private readonly static int Parallelism = 4;
+    private readonly static int Iterations = 4;
+
+    public static string GenerateHash(string password)
+    {
+        var salt = RandomNumberGenerator.GetBytes(16);
+
+        var argon = new Argon2id(Encoding.UTF8.GetBytes(password))
+        {
+            Salt = salt,
+            MemorySize = Memory,
+            DegreeOfParallelism = Parallelism,
+            Iterations = Iterations
+        };
+
+        var passwordHash = argon.GetBytes(32);
+
+        return Convert.ToBase64String(salt.Concat(passwordHash).ToArray());
+    }
+
+    public static bool VerifyHash(string passwordHash, string password)
+    {
+        var decodifation = Convert.FromBase64String(passwordHash);
+
+        var salt = decodifation.Take(16).ToArray();
+
+        var storeHash = decodifation.Skip(16).ToArray();
+
+        var argon = new Argon2id(Encoding.UTF8.GetBytes(password))
+        {
+            Salt = salt,
+            MemorySize = Memory,
+            DegreeOfParallelism = Parallelism,
+            Iterations = Iterations
+        };
+
+        var hash = argon.GetBytes(32);
+
+        return CryptographicOperations.FixedTimeEquals(hash, storeHash);
+    }
+}
