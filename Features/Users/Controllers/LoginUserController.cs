acf056d62d46ffb94d8ea5dc9387bb3a3d6adb07
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-12-23 17:23:49 -0300

    refactor: melhorias no status code da requisição

--- Features/Users/Controllers/LoginUserController.cs
+++ Features/Users/Controllers/LoginUserController.cs
@@ -18,12 +18,12 @@ public sealed class LoginUserController(AppDbContext context) : ControllerBase
     public async Task<IActionResult> ExecuteAsync([FromBody] LoginUserDto loginDto, CancellationToken cancellationToken = default)
     {
         if (!ModelState.IsValid)
-            return BadRequest(ModelState);
+            return UnprocessableEntity(ModelState);
 
         var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email, cancellationToken: cancellationToken);
 
         if (user is null || !Hasher.VerifyHash(user.Password, loginDto.Password))
-            return BadRequest("");
+            return Unauthorized(new { message = "Invalid credentials." });
 
         return Ok(new { message = "Login successfully.", token = user.GenerateToken() });
     }
