4bb5bb0d9ee8aaec46e004f0cdef1a85518e7194
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-12-23 17:26:26 -0300

    feat: cria o controller para deletar conta do usuário

---
+++ Features/Users/Controllers/DeleteUserController.cs
@@ -0,0 +1,39 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stokify.Data.Context;
+using Stokify.Features.Users.Auth;
+using Stokify.Features.Users.Dto;
+
+namespace Stokify.Features.Users.Controllers;
+
+[ApiController]
+[Route("v1/user/delete")]
+[Tags("Delete")]
+public sealed class DeleteUserController(AppDbContext context) : ControllerBase
+{
+    [Authorize]
+    [HttpDelete]
+    public async Task<IActionResult> ExecuteAsync([FromBody] ConfirmPasswordDto passwordDto, CancellationToken cancellationToken = default)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (!Guid.TryParse(id, out var userId))
+            return Unauthorized(new { message = "Invalid user identifier." });
+
+        var user = await context.Users.SingleOrDefaultAsync(x => x.Id == userId, cancellationToken);
+
+        if (user is null || !Hasher.VerifyHash(user.Password, passwordDto.ConfirmPassword))
+            return Unauthorized(new { message = "Invalid credentials." });
+
+        context.Users.Remove(user);
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        return NoContent();
+    }
+}
