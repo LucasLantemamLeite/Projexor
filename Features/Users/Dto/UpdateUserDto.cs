9d282da5968f109cfbf9911e64776db09917565c
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-12-23 17:26:13 -0300

    feat: cria o dto para atualizar dados de um usuário herdando do ConfirmPassword

---
+++ Features/Users/Dto/UpdateUserDto.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Stokify.Features.Users.Dto;
+
+public sealed record UpdateUserDto : ConfirmPasswordDto
+{
+    [Required(ErrorMessage = "Name is required.")]
+    [MaxLength(100, ErrorMessage = "Name must be between 3 and 100 characters.")]
+    public required string Name { get; init; }
+
+    [Required(ErrorMessage = "Email address is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address format.")]
+    [MaxLength(255, ErrorMessage = "Email must not exceed 255 characters.")]
+    public required string Email { get; init; }
+
+    [Required(ErrorMessage = "Phone number is required.")]
+    [Phone(ErrorMessage = "Invalid phone number format.")]
+    [MaxLength(15, ErrorMessage = "Phone number most exceed 15 characters.")]
+    public required string Phone { get; init; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [StringLength(30, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 30 characters.")]
+    public required string Password { get; init; }
+
+    [Required(ErrorMessage = "Active is required.")]
+    public bool Active { get; init; }
+}
