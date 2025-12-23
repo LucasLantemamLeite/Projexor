d1095bbe23fa0f82938cd01b7b4994404c12771a
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-12-22 23:08:42 -0300

    feat: cria os enumeradores das entidades

---
+++ Shared/Enums/ERole.cs
@@ -0,0 +1,7 @@
+namespace Stokify.Shared.Enums;
+
+public enum ERole
+{
+    Member = 1,
+    Owner = 2
+}
---
+++ Shared/Enums/EStatus.cs
@@ -0,0 +1,9 @@
+namespace Stokify.Shared.Enums;
+
+public enum EStatus
+{
+    InProgress = 1,
+    Completed = 2,
+    Delayed = 3,
+    Finished = 4
+}
