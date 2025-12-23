b0a59fdaa3d12aa66c26e640feebf685a4982fef
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-12-23 11:20:00 -0300

    feat: altera acesso para público

--- Shared/Base/Entity.cs
+++ Shared/Base/Entity.cs
@@ -2,12 +2,11 @@ namespace Stokify.Shared.Base;
 
 public abstract class Entity
 {
-    protected Guid Id { get; set; }
-    
+    public Guid Id { get; protected set; }
+
     protected Entity()
         => Id = Guid.NewGuid();
-    
-    // ReSharper disable once ConvertToPrimaryConstructor
+
     protected Entity(Guid id)
-        => Id = id;
+    => Id = id;
 }
