206981e26b85bf5fca055b1144d030772ab72fcf
LucasLantemamLeite <lucaslantemam2005@gmail.com>
2025-12-23 13:23:01 -0300

    feat: cria o middleware para centralizar todas as Exceptions

---
+++ Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,26 @@
+namespace Stokify.Middlewares;
+
+public sealed class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext http)
+    {
+        try
+        {
+            await next(http);
+        }
+
+        catch (OperationCanceledException) { }
+
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Exception Error.");
+
+            http.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            http.Response.ContentType = "application/json";
+
+            var response = new { message = "Internal server error. Try again later." };
+
+            await http.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
